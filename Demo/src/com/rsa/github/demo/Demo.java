922f945477a9dbb7e5e2974835cd0f06c5517820
Aksharaa Sundarrajan <aksharaa.aiesec@gmail.com>
2020-04-16 02:29:09 +0530

    1st commit

---
+++ Demo/src/com/rsa/github/demo/Demo.java
@@ -0,0 +1,21 @@
+package com.rsa.github.demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Github demo project");
+		
+		int sum = 1+1;
+		
+		if (sum == 2) {
+			System.out.print("The sum is 2!");
+		}
+		else
+		{
+			System.out.print("The sum is not 2");
+		}
+		
+
+	}
+
+}
