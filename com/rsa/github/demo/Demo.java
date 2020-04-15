b97a2e43799f1e4bad3fe6fb713f541e0080441b
Aksharaa <Aksharaa@LAPTOP-L8LKEEJH>
2020-04-16 02:44:43 +0530

    Made changes to logic.

---
+++ com/rsa/github/demo/Demo.java
@@ -0,0 +1,21 @@
+package com.rsa.github.demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Github demo project");
+		
+		int sum = 1+3;
+		
+		if (sum == 4) {
+			System.out.print("The sum is 4!");
+		}
+		else
+		{
+			System.out.print("The sum is not 4");
+		}
+		
+
+	}
+
+}
